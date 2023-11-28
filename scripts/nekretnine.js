dcd2d274f00f2608f915764dd0ecab8f567f9467
Amna Hodžić <ahodzic1@etf.unsa.ba>
2023-11-28 16:46:36 +0100

    small fixes

--- scripts/nekretnine.js
+++ scripts/nekretnine.js
@@ -20,11 +20,19 @@ function spojiNekretnine(divReferenca, instancaModula, tip_nekretnine) {
 
   filtriraneNekretnine.forEach(nekretnina => {
     // Kreiranje HTML elementa za prikaz nekretnine
-    const nekretninaElement = createElement('div', 'property', null);
+    const nekretninaElement= document.createElement('div');
+     
+    if(tip_nekretnine=="Stan")
+    nekretninaElement.classList.add("property", "stan");
+  else if( tip_nekretnine=="Kuća")
+   nekretninaElement.classList.add("property", "kuca");
+  else if (tip_nekretnine=="Poslovni prostor")
+  nekretninaElement.classList.add("property", "pp");
 
     const imgElement = createElement('img', null, null);
-    imgElement.src = nekretnina.slika;
+    imgElement.src = "https://www.apartments.com/blog/sites/default/files/styles/x_large_hq/public/image/2023-06/ParkLine-apartment-in-Miami-FL.jpg?itok=kQmw64UU";
     imgElement.alt = tip_nekretnine;
+    
 
     const propertyInfoElement = createElement('div', 'property-info', null);
 
@@ -47,7 +55,72 @@ function spojiNekretnine(divReferenca, instancaModula, tip_nekretnine) {
   });
 
   // Append the container element to divReferenca
-  divReferenca.innerHTML = ''; // Clear the content
+  if(divReferenca){
   divReferenca.appendChild(h2Element);
   divReferenca.appendChild(containerElement);
+  }
 }
+
+const divStan = document.getElementById("stan");
+const divKuca = document.getElementById("kuca");
+const divPp = document.getElementById("pp");
+
+const listaNekretnina = [{
+    id: 1,
+    tip_nekretnine: "Stan",
+    naziv: "Useljiv stan Sarajevo",
+    kvadratura: 58,
+    cijena: 232000,
+    tip_grijanja: "plin",
+    lokacija: "Novo Sarajevo",
+    godina_izgradnje: 2019,
+    datum_objave: "01.10.2023.",
+    opis: "Sociis natoque penatibus.",
+    upiti: [{
+        korisnik_id: 1,
+        tekst_upita: "Nullam eu pede mollis pretium."
+    },
+    {
+        korisnik_id: 2,
+        tekst_upita: "Phasellus viverra nulla."
+    }]
+},
+{
+    id: 2,
+    tip_nekretnine: "Poslovni prostor",
+    naziv: "Mali poslovni prostor",
+    kvadratura: 20,
+    cijena: 70000,
+    tip_grijanja: "struja",
+    lokacija: "Centar",
+    godina_izgradnje: 2005,
+    datum_objave: "20.08.2023.",
+    opis: "Magnis dis parturient montes.",
+    upiti: [{
+        korisnik_id: 2,
+        tekst_upita: "Integer tincidunt."
+    }
+    ]
+}]
+
+const listaKorisnika = [{
+    id: 1,
+    ime: "Neko",
+    prezime: "Nekic",
+    username: "username1",
+},
+{
+    id: 2,
+    ime: "Neko2",
+    prezime: "Nekic2",
+    username: "username2",
+}]
+
+//instanciranje modula
+let nekretnine = SpisakNekretnina();
+nekretnine.init(listaNekretnina, listaKorisnika);
+
+//pozivanje funkcije
+spojiNekretnine(divStan, nekretnine, "Stan");
+spojiNekretnine(divKuca, nekretnine, "Kuća");
+spojiNekretnine(divPp, nekretnine, "Poslovni prostor");
