be8f4a87c2d3b38e67e5cea4ea9d7a8f9b3b3207
Amna Hodžić <ahodzic1@etf.unsa.ba>
2024-01-21 15:55:49 +0100

    small changes

--- public/scripts/spojiUpite.js
+++ public/scripts/spojiUpite.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const postaviUpitDiv = document.getElementById('postavi-upit');
     const tekstUpitaInput = document.getElementById('tekst-upita');
     const postaviUpitBtn = document.getElementById('postavi-upit-btn');
-   
+    const upitiContainer = document.getElementById('upiti');
 
     PoziviAjax.getKorisnik(function (err, data) {
         if (!data) {
@@ -22,8 +22,16 @@ document.addEventListener('DOMContentLoaded', function () {
                     if (err) {
                         console.error('Greška prilikom slanja upita:', err);
                     } else {
-                        const detaljiUrl = `detalji.html?id=${nekretninaId}`;
-                        window.location.href = detaljiUrl;
+                        const newUpit = document.createElement('li');
+                        newUpit.className = 'upit';
+                        newUpit.innerHTML = `
+                            <p class="username">${data.username}</p>
+                            <p class="sadrzajupita">${tekstUpita}</p>
+                        `;
+                        upitiContainer.appendChild(newUpit);
+
+                        // Clear the input field after posting the query
+                        tekstUpitaInput.value = '';
                     }
                 });
             });
